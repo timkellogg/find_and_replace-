55888309be0cede61bf4b6d97e74ac2f4edf393a
Julianna Green & Tim Kellogg <student@epicodus.com>
2015-08-05 11:54:01 -0700

    improved method by searching globally

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,9 +1,7 @@
 class String
 
   define_method(:find_and_replace) do |find, replace|
-    # Looks in the text the user put in, finds the letters that match what the user
-    # put in for the first argument, and changes those to what the user put in for
-    # the second argument.
-    self.gsub(find, replace)
+
+    self.gsub!(find, replace)
   end
 end
